2a4254f32463ea59604e7e5a8c9757699d7ec491
sh-an-kar-an <80773629+sh-an-kar-an@users.noreply.github.com>
2024-10-02 17:13:32 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,3 +1,8 @@
+# Django signals are executed synchronously. When a signal is sent,
+# the code that sends the signal will wait for all connected receivers to finish executing before continuing.
+# This ensures that the signal is fully processed before the code that sent the signal continues to run.
+
+
 import time
 from django.dispatch import Signal
 
