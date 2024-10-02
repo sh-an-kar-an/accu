fd91d295d530b603c2b124d1d4f601d9410dee07
sh-an-kar-an <80773629+sh-an-kar-an@users.noreply.github.com>
2024-10-02 17:28:27 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -3,14 +3,14 @@ class Rectangle:
         self.length = length
         self.width = width
 
-    # Make the class iterable
+   
     def __iter__(self):
         yield {'length': self.length}
         yield {'width': self.width}
 
-# Example usage:
+
 rect = Rectangle(10, 5)
 
-# Iterate over the instance
+
 for attr in rect:
     print(attr)
